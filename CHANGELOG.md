dc412aa6cba0ba28ec65782d2387c36909ac76cf
Steve Lewis <steve.l@lojistic.com>
2019-12-02 11:49:05 -0800

    Allow optional request parameters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 0.2.2 (2019-12-02)
+
+* Support optional request args.
+
 ### 0.1.2 (2019-09-20)
 
 * Changed module names and paths to make auto-inclusion work properly
--- lib/darksky_weather/api/client.rb
+++ lib/darksky_weather/api/client.rb
@@ -6,11 +6,18 @@ module DarkskyWeather
       include HTTParty
       base_uri "https://api.darksky.net/forecast/"
 
-      def get_weather(lat:, lng:, timestamp: nil)
+      def get_weather(lat:, lng:, timestamp: nil, opts: {})
         key         = DarkskyWeather::Api.configuration.api_key
 
         request_path   = "/#{key}/#{lat},#{lng}"
         request_path << ",#{timestamp}" if timestamp
+
+        allowed_opts = %w(exclude lang units)
+        opts.each_with_index do |o, idx|
+          next unless allowed_opts.include?(o.first.to_s)
+          sep_sym = idx == 0 ? '?' : '&'
+          request_path << "#{sep_sym}#{o.first}=#{o.last}"
+        end
 
         raw_result  = self.class.get(request_path)
         request_uri = raw_result.request.uri.to_s
